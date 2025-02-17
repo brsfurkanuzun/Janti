e0000b2babde4b1358253659ecf8f90785cef2a5
Furkan Uzun <126771644+brsfurkanuzun@users.noreply.github.com>
2025-02-17 17:01:59 +0300

    Menu editted

--- Janti/src/components/Navbar/index.jsx
+++ Janti/src/components/Navbar/index.jsx
@@ -8,10 +8,20 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <button className="menu-button" onClick={() => setMenuOpen(true)}>MENU</button>
-      <Link to="/" className="logo">
-        <img src="/orjLogo.png" alt="Logo" draggable="false" />
-      </Link>
+      <div className="top-item top-item-one">
+        <a onClick={() => setMenuOpen(true)}>
+          MENU
+        </a>
+      </div>
+      <div className="top-item top-item-two">
+        <Link to="/" className="logo">
+          <img src="/orjLogo.png" alt="Logo" draggable="false" />
+        </Link>
+      </div>
+      <div className="top-item top-item-three">
+        <Link to="/contact">İLETİŞİM</Link>
+      </div>
+
       <Menu isOpen={menuOpen} closeMenu={() => setMenuOpen(false)} />
     </div>
   );
